61b8387542f9644f81c3399938f62c1a444fb9b5
simplyDonald <donaldabuah@gmail.com>
2022-11-26 00:02:27 -0500

    edit header option styling

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,12 @@ import React from 'react'
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
 import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+  import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+  import ChatIcon from '@mui/icons-material/Chat';
+  import NotificationsIcon from '@mui/icons-material/Notifications';
+
 
 function Header() {
   return (
@@ -18,8 +24,11 @@ function Header() {
       </div>
 
       <div className="header__right">
-        <HeaderOption title="Home" />
-        <HeaderOption title="My Network" />
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
       </div>
 
     </div>
