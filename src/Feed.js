aebb4f071c5f9a2dbb8973fb2146a75f0c7009cc
simplyDonald <donaldabuah@gmail.com>
2022-11-27 22:22:16 -0500

    add input options icons with custom colors

--- src/Feed.js
+++ src/Feed.js
@@ -3,6 +3,9 @@ import './Feed.css';
 import CreateIcon from '@mui/icons-material/Create';
 import InputOption from './InputOption';
 import ImageIcon from '@mui/icons-material/Image';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
 
 function Feed() {
   return (
@@ -18,8 +21,10 @@ function Feed() {
 
         <div className="feed__inputOptions">
           <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+          <InputOption Icon={EventNoteIcon } title="Event" color="#C0CBCD" />
           
-
+          <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7FC15E" />
         </div>
       </div>
 
