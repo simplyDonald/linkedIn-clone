dd605c55a76530c749cfad0e71800a9c7d1f3fd4
simplyDonald <donaldabuah@gmail.com>
2022-11-27 20:40:16 -0500

    complete format for sidebar styling

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div className="app__body">
         <Sidebar />
         {/* Sidebar */}
-        {/* Feed */}
+        {/* Feed */}                                              
         {/* Widgets */}
       </div>
     </div>
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,10 +3,18 @@ import React from 'react'
 import './Sidebar.css';
 
 function Sidebar() {
+
+  const recentItem = (topic) => (
+  <div className="sidebar__recentItem">
+    <span className="sidebar__hash">#</span>
+    <p>{topic}</p>
+  </div>
+  )
+
   return (
     <div className='sidebar'>
       <div className='sidebar__top'>
-        <img src="https://images.unsplash.com/photo-1669458984644-d7186f312fa9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+        <img src="https://images.unsplash.com/photo-1669287731461-bd8ce3126710?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" alt="" />
         <Avatar className='sidebar__avatar' />
         <h2>Donald Abuah</h2>
         <h4>donspaceyou@yahoo.com</h4>
@@ -23,9 +31,16 @@ function Sidebar() {
           <p className="sidebar__statNumber">800</p>
         </div>
 
-        <div className="sidebar__button">
-          <p>Recent</p>
-        </div>
+      </div>
+
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+        {recentItem('reactjs')}
+        {recentItem('programming')}
+        {recentItem('softwareengineering')}
+        {recentItem('design')}
+        {recentItem('developer')}
+
       </div>
     </div>
   )
