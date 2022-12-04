4fc66a1f046d567f5f16b8e1a3b0508ebf1a9b21
simplyDonald <donaldabuah@gmail.com>
2022-12-03 21:46:53 -0500

    add user variable from userSlice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
 import './App.css';
+import { selectUser } from './features/userSlice';
 import Feed from './Feed';
 import Header from './Header';
+import Login from './Login';
 import Sidebar from './Sidebar';
 
 function App() {
+
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header />
 
-      {/* App Body */}
+      {!user ? (<Login />) :  (
       <div className="app__body">
         <Sidebar />
         {/* Sidebar */}
         {/* Feed */}
         <Feed />                                              
         {/* Widgets */}
-      </div>
+      </div>)}
+      
     </div>
   );
 }
