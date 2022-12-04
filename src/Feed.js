893f4591e4152a6e0110d36b3e98720b2830c6a1
simplyDonald <donaldabuah@gmail.com>
2022-12-04 01:33:47 -0500

    add flip move package and wrap comps. in feed

--- src/Feed.js
+++ src/Feed.js
@@ -11,6 +11,7 @@ import {db} from './firebase';
 import firebase from 'firebase';
 import { useSelector } from 'react-redux';
 import { selectUser } from './features/userSlice';
+import FlipMove from 'react-flip-move';
 // V9 firebase                               // 
 // import { addDoc, collection } from 'firebase/firestore';
 
@@ -76,12 +77,15 @@ function Feed() {
         </div>
       </div>
 
-      {/* Post */}
+      {/* Post */}\
+      <FlipMove>
+
       {/* map through the state's posts array */}
       {posts?.map(({id, data:{name, description,message, photoUrl}}) => (
         <Post key={id} name={name} description={description} message={message} photoUrl={photoUrl} />
 
       ))}
+      </FlipMove>
     </div>
   )
 }
