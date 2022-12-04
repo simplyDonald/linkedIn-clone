eb0478b5e8f274a5c72b79c6473341fd63c9c977
simplyDonald <donaldabuah@gmail.com>
2022-12-04 00:06:37 -0500

    setup logout functionality

--- src/Header.js
+++ src/Header.js
@@ -7,9 +7,21 @@ import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
   import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
   import ChatIcon from '@mui/icons-material/Chat';
   import NotificationsIcon from '@mui/icons-material/Notifications';
+import { auth } from './firebase';
+import { useDispatch } from 'react-redux';
+import { logout } from './features/userSlice';
 
 
 function Header() {
+  const dispatch = useDispatch();
+
+  const logoutOfApp = () => {
+    // logout function call for firebase auth
+    auth.signOut();
+    // dispatch the logout action to your redux store
+    dispatch(logout());
+  };
+
   return (
     <div className="header">
       
@@ -29,7 +41,7 @@ function Header() {
         <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
         <HeaderOption Icon={ChatIcon} title="Messaging" />
         <HeaderOption Icon={NotificationsIcon} title="Notifications" />
-        <HeaderOption avatar={"https://randomwordgenerator.com/img/picture-generator/51e4d7444c54b10ff3d8992cc12c30771037dbf85254794e73277bd69e4f_640.jpg"} title="Me" />
+        <HeaderOption avatar={"https://randomwordgenerator.com/img/picture-generator/51e4d7444c54b10ff3d8992cc12c30771037dbf85254794e73277bd69e4f_640.jpg"} title="Me" onClick={logoutOfApp} />
 
       </div>
 
