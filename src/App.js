a8964c66f428eae7327050be6e6f4f86775a42da
simplyDonald <donaldabuah@gmail.com>
2022-12-04 23:44:16 -0500

    setup widgets section

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { auth } from './firebase';
 import Header from './Header';
 import Login from './Login';
 import Sidebar from './Sidebar';
+import Widgets from './Widgets';
 
 function App() {
 
@@ -41,6 +42,7 @@ function App() {
         {/* Feed */}
         <Feed />                                              
         {/* Widgets */}
+        <Widgets />
       </div>)}
       
     </div>
