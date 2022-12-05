bae88145ed7baaf3c0f9ba4be1801d10a6b08a7f
simplyDonald <donaldabuah@gmail.com>
2022-12-05 01:36:24 -0500

    increase input field size

--- src/Header.js
+++ src/Header.js
@@ -32,7 +32,7 @@ function Header() {
 
         <div className="header__search">
           <SearchIcon />
-          <input type="text" />
+          <input type="text" placeholder='Search' />
 
         </div>
       </div>
