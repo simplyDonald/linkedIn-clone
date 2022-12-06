fbcf04e0fc9550ec53c86f9db9ea17f7bd4fe019
simplyDonald <donaldabuah@gmail.com>
2022-12-06 14:46:45 -0500

    add code for dynamic header right

--- src/Header.js
+++ src/Header.js
@@ -36,16 +36,16 @@ function Header() {
 
         </div>
       </div>
-
-      <div className="header__right">
+      {user && ( <div className="header__right">
         <HeaderOption Icon={HomeIcon} title="Home"  />
         <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
         <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
         <HeaderOption Icon={ChatIcon} title="Messaging" />
         <HeaderOption Icon={NotificationsIcon} title="Notifications" />
-        <HeaderOption avatar={true} title="Me" onClick={logoutOfApp} />
+        <HeaderOption avatar={true} title={user.displayName} onClick={logoutOfApp} />
 
-      </div>
+      </div>)}
+     
 
     </div>
     
